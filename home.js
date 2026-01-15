a1278ec09a4b11e92fdd53eb8f373f3ac355d51f
AAriif261 <iliketurtles1233au855@gmail.com>
2026-01-15 12:18:29 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,21 +1,39 @@
-<script>
-    function addAnime() {
-      const input = document.getElementById("animeInput");
-      const list = document.getElementById("animeList");
+console.log("js console"); 
 
-      if (input.value.trim() !== "") {
-        do {
-	function name(params) {
-    const
-  }
-function name(params) {
-	array
-}.forEach(element ={">"} {
-	}
-{"}"}); while (condition); li = document.createElement("li");
-        li.textContent = input.value;
-        list.appendChild(li);
-        input.value = "";
-      {"}"}
-    {"}"}
-  </script>
+let data; // [__N__] - 2
+let grid = document.querySelector(".grid-container"); 
+
+var xhttp = new XMLHttpRequest(); 
+
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) { 
+
+       data = JSON.parse(xhttp.responseText); 
+       console.log(data); 
+
+       data.forEach(function(anime) { 
+        let card = document.createElement("div"); 
+        card.classList.add("card"); 
+
+        let textData =
+          "<div class='anime-title'>" + anime.title + "</div>" +
+          "<span>" +
+          "Publisher: " + anime.publisher + "<br>" +
+          " Release Date: " + anime.releaseDate + "<br>" +
+          "Needs Research: " +
+          "</span>";
+
+        card.innerHTML = textData; //
+
+        if (anime.imgSrc) { 
+            card.style.backgroundImage = "url('" + anime.imgSrc + "')";
+        }
+
+        grid.appendChild(card); 
+       });
+
+    }
+};
+
+xhttp.open("GET", "anime.json", true); // [__I__] - 16
+xhttp.send(); // [__B__] - 17
